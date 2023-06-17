08a119dc6efe32f2e4942a64ffbcdc57c1c355f8
hieubt <104885277+hieubt1@users.noreply.github.com>
2023-06-17 08:56:30 +0700

    Change link to download xip

--- get_xip.sh
+++ get_xip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-curl -SL https://github.com/xplor-studio/xip/releases/latest/download/xip -o /tmp/xip
+curl -SL https://github.com/hieubt1/xip/releases/latest/download/xip -o /tmp/xip
 chmod +x /tmp/xip
 sudo mv /tmp/xip /usr/local/bin/xip
